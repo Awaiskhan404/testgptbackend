5eb1126af4a40fcf96f4cd10ce451152ebaec660
Awais khan <contact@awaiskhan.com.pk>
2025-01-23 17:25:42 +0500

    Add CORS middleware to main.py for cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import health, chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers
 app.include_router(health.router)
